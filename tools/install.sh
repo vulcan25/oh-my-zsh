a8f9ad7e5f2682c132e156ae34a89901a5d1a67a
jonny <>
2019-01-20 19:01:35 +0000

    changed install script to make it not chsh

--- tools/install.sh
+++ tools/install.sh
@@ -84,7 +84,7 @@ main() {
     # If this platform provides a "chsh" command (not Cygwin), do it, man!
     if hash chsh >/dev/null 2>&1; then
       printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
+      #chsh -s $(grep /zsh$ /etc/shells | tail -1)
     # Else, suggest the user do so manually.
     else
       printf "I can't change your shell automatically because this system does not have chsh.\n"
@@ -108,7 +108,6 @@ main() {
   echo 'p.p.s. Get stickers and t-shirts at https://shop.planetargon.com.'
   echo ''
   printf "${NORMAL}"
-  env zsh -l
 }
 
 main
